75aee4caff1bdfdb40c391ae79b8d442b0cfaac2
ochhanejay <ochhanejay17@gmail.com>
2022-11-06 16:19:58 +0530

    study

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar/navbar';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle"
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/courses/courses.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+const Course = () => {
+    return ( <div>
+        
+        </div> );
+}
+ 
+export default Course;
